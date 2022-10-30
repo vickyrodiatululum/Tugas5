2cfe8a7edc777f749cf8f2b2c4da15ece5861090
vickyrodiatululum <114964366+vickyrodiatululum@users.noreply.github.com>
2022-10-30 22:39:42 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#0EC5D7',
+                secondary: '#ff3b07',
+                nav: '#969696',
+                header: '#F6F9FC',
+            },
+            fontFamily: {
+                raleway: ['Raleway', 'sans-serif'],
+                nunito: ['Nunito Sans', 'sans-serif'],
+            }
+        },
+    },
+}
